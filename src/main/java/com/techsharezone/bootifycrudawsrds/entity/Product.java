346b18dd94fd0c8743d8af233ee45f578f0ee7e9
SAURABH SHARMA <saurabhshcs@gmail.com>
2021-01-20 18:55:23 +0000

    Update Product.java

--- src/main/java/com/techsharezone/bootifycrudawsrds/entity/Product.java
+++ src/main/java/com/techsharezone/bootifycrudawsrds/entity/Product.java
@@ -29,44 +29,4 @@ public class Product {
     private int qty;
     private double price;
     private String creationTS;
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public int getQty() {
-        return qty;
-    }
-
-    public void setQty(int qty) {
-        this.qty = qty;
-    }
-
-    public double getPrice() {
-        return price;
-    }
-
-    public void setPrice(double price) {
-        this.price = price;
-    }
-
-    public String getCreationTS() {
-        return creationTS;
-    }
-
-    public void setCreationTS(String creationTS) {
-        this.creationTS = creationTS;
-    }
 }
